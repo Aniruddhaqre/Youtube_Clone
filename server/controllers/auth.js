51879585ab9d16067370aef558058be8fe2c6eaa
Aniruddhaqre <aniruddhamestryqre@gmail.com>
2023-07-30 23:54:39 +0530

    third

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs"
 import User from "../models/User.js";
+import { createError } from "../error.js";
 
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
 
     try {
         const salt = await bcrypt.genSalt(10);
@@ -13,6 +14,22 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(200).send("User has been created")
     } catch (error) {
-        //todo
+        next(error)
     }
 };
+
+export const signin = async(req, res, next) => {
+    try {
+        const user = await User.findOne({name : req.body.name})
+        
+        if(!user) return next(createError(404, "User not found" ));
+
+        const isCorrect = await bcrypt.compare(req.body.password, user.password );
+
+        if(!isCorrect) return next(createError(400 , "Wrong credentials"));
+
+        
+    } catch (error) {
+        
+    }
+}
