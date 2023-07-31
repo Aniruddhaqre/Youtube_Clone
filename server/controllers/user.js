f150f1a218fc039b67bc25c49d1e1950dda34ec8
Aniruddhaqre <aniruddhamestryqre@gmail.com>
2023-07-31 23:48:48 +0530

    fourth

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,21 @@
-export const  test = async(req , res) => {
-    console.log("test is working")
-    res.send({
-        sucess : true,
-    })
+export const update = async(req , res, next) => {
+    
+}
+export const deleteUser = async(req , res, next) => {
+    
+}
+export const getUser = async(req , res, next) => {
+    
+}
+export const subscribe = async(req , res, next) => {
+    
+}
+export const unsubscribe = async(req , res, next) => {
+    
+}
+export const like = async(req , res, next) => {
+    
+}
+export const dislike = async(req , res, next) => {
+    
 }
